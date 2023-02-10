b7a868e9f3c801c261b6b91fd43eb17f30e8135a
teenteennnii <teennopwijit@gmail.com>
2023-02-10 10:15:29 +0700

    add ldr code

---
+++ src/ldr.h
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#define LDR 32
+void setup() {
+    Serial.begin(115200);
+    Serial.println("LDR");
+}
+
+void loop() {
+    Serial.println(analogRead(LDR));
+    delay(100);
+}
