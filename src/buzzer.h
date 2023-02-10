78d4143b273100c78e59e5c3fcef97aaf9a54021
teenteennnii <teennopwijit@gmail.com>
2023-02-10 10:17:13 +0700

    update buzzer

--- src/buzzer.h
+++ src/buzzer.h
@@ -1,20 +1,21 @@
 #include <Arduino.h>
 
-#define buzzer 12 // set the buzzer control digital IO pin
+#define buzzer 12
 void setup() {
-	pinMode(buzzer, OUTPUT); // set pin 8 as output
+	pinMode(buzzer, OUTPUT);
 }
 void loop() {
-	for (int i = 0; i < 100; i++) {  // make a sound
-		digitalWrite(buzzer, HIGH); // send high signal to buzzer 
-		delay(1); // delay 1ms
-		digitalWrite(buzzer, LOW); // send low signal to buzzer
+	for (int i = 0; i < 100; i++) 
+    {
+		digitalWrite(buzzer, HIGH);
+		delay(1);
+		digitalWrite(buzzer, LOW);
 		delay(1);
 	}
 	delay(50);
-	for (int j = 0; j < 100; j++) { //make another sound
+	for (int j = 0; j < 100; j++) {
 		digitalWrite(buzzer, HIGH);
-		delay(2); // delay 2ms
+		delay(2);
 		digitalWrite(buzzer, LOW);
 		delay(2);
 	}
