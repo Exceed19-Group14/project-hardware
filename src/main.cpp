0365981be70c25b5703c21967f2520906943d737
teenteennnii <teennopwijit@gmail.com>
2023-02-10 16:28:52 +0700

    connect to backend

--- src/main.cpp
+++ src/main.cpp
@@ -21,10 +21,10 @@ DallasTemperature sensors(&oneWire);
 int StopSong = 0;
 int ClickToStart = 0;
 
-const String baseUrl = "WEB";
+const String baseUrl = "http://group14.exceed19.online/";
 
-const char *ssid = "<Wifi name>";
-const char *password = "<Wifi password>";
+const char *ssid = "group15";
+const char *password = "thisisapassword";
 
 void Connect_Wifi() {
     WiFi.begin(ssid, password);
@@ -40,52 +40,62 @@ void Connect_Wifi() {
 int On_Off_Auto = 0;
 long Dulation = 1000;
 
-void GET_post(){
+void GET_post(void *param){
+    while(true){
     DynamicJsonDocument doc(2048);
-    const String url = baseUrl + "...";
+    const String url = baseUrl + "plant/1/water";
     HTTPClient http;
     http.begin(url);
     int httpResponseCode = http.GET();
     if (httpResponseCode >= 200 && httpResponseCode < 400) {
-        Serial.print("HTTP ");
-        Serial.println(httpResponseCode);
+        Serial.print("GET!!");
         String payload = http.getString();
         deserializeJson(doc, payload);
-        On_Off_Auto = doc["id"].as<int>();
+        ClickToStart = doc["water_status"].as<int>();
+        Dulation = doc["duration"].as<int>();
+        Serial.printf("Status : %d, Dulation : %d\n", ClickToStart, Dulation);
+        if (ClickToStart == 1){
+            vTaskDelay(Dulation/portTICK_PERIOD_MS);
+        }
+        Serial.println("=======================");
     }
     else {
         Serial.print("Error code: ");
         Serial.println(httpResponseCode);
+    }
+    vTaskDelay(5000/portTICK_PERIOD_MS);
     }
 }
 
 int light = 0;
 int moisture = 0;
 float temp = 0.0;
-int id = 0;
 
-void PUT_data(){
+void PATCH_data(void *param){
+    while(true){
+    vTaskDelay(20000/portTICK_PERIOD_MS);
     String json;
     DynamicJsonDocument doc(2048);
-    doc["Id"] = id;
     doc["light"] = light;
     doc["moisture"] = moisture;
-    doc["temp"] = temp;
+    doc["temperature"] = temp;
     serializeJson(doc,json);
 
-    const String url = baseUrl + "...";
+    const String url = baseUrl + "plant/1";
     HTTPClient http;
     http.begin(url);
     http.addHeader("Content-Type","application/json");
 
-    int httpResponseCode = http.PUT(json);
+    int httpResponseCode = http.PATCH(json);
     if (httpResponseCode >= 200 && httpResponseCode < 300) {
-        Serial.print("HTTP ");
-        Serial.println(httpResponseCode);
+        Serial.print("PATCH COMPLETE !!");
+        Serial.printf("Light : %d , Moiture : %d , Temp : %.2f°C\n", light, moisture, temp);
+        Serial.println("=======================");
     }
     else {
         Serial.print("Error code: ");
         Serial.println(httpResponseCode);
+    }
     }
 }
 
@@ -117,18 +127,14 @@ void PlaySong(){
         delay(1);
         digitalWrite(BUZZER, LOW);
         delay(1);
-        // ต้องเพิ่ม force stop
 	}
 }
-
-int IndexMeasure = 0;
 
 void Measure(){
     sensors.requestTemperatures();
     light = analogRead(LDR);
     moisture = analogRead(SOIL);
     temp = sensors.getTempCByIndex(0);
-    IndexMeasure++;
 
     if (moisture < 300){
         ledcWrite(0, 200);
@@ -138,8 +144,6 @@ void Measure(){
         ledcWrite(0, 0);
         ledcWrite(1, 70);
     }
-
-    Serial.printf("Index : %d | Light : %d , Moiture : %d , Temp : %.2f°C\n",IndexMeasure, light, moisture, temp);
     delay(2000);
 }
 
@@ -147,7 +151,6 @@ void Start(void *param){
     while(true){
     Measure();
     if (On_Off_Auto == 1 && moisture < 300){
-        Serial.println("=======================");
         Serial.println("Initiated!! Auto Mode.");
         Serial.printf("Dulation = %d Secound.\n", Dulation/1000);
         PlaySong();
@@ -155,8 +158,7 @@ void Start(void *param){
         Serial.println("=======================");
     }
     if (On_Off_Auto == 0 && ClickToStart == 1){
-        Serial.println("=======================");
-        Serial.println("Initiated!! Manual Mode.");
+        Serial.println("Initiated!! Start AquaBot!!");
         Serial.printf("Dulation = %d Secound.\n", Dulation/1000);
         PlaySong();
         Serial.println("Done.");
@@ -175,10 +177,11 @@ void setup() {
     ledcSetup(1, 5000, 8);
     ledcAttachPin(green_LED, 1);
 
-    ClickToStart = 1;
-
+    Connect_Wifi();
+    
     xTaskCreatePinnedToCore(Start, "StartProgram", 10000, NULL, 1, &TaskA, 0);
-
+    xTaskCreatePinnedToCore(GET_post, "GET_post", 10000, NULL, 1, &TaskA, 1);
+    xTaskCreatePinnedToCore(PATCH_data, "PATCH_data", 10000, NULL, 1, &TaskA, 1);
 }
 
 void loop() {
