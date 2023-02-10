321e859e16037dbb6315e16a63070348756a9e64
teenteennnii <teennopwijit@gmail.com>
2023-02-10 11:38:53 +0700

    add simple get/put

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,57 @@ void Connect_Wifi() {
     Serial.println(WiFi.localIP());
 }
 
+int On_Off = 0;
+
+void GET_post(){
+    DynamicJsonDocument doc(2048);
+    const String url = baseUrl + "...";
+    HTTPClient http;
+    http.begin(url);
+    int httpResponseCode = http.GET();
+    if (httpResponseCode >= 200 && httpResponseCode < 400) {
+        Serial.print("HTTP ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        deserializeJson(doc, payload);
+        On_Off = doc["id"].as<int>();
+    }
+    else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+    }
+}
+
+int light = 0;
+int moisture = 0;
+float temp = 0.0;
+int id = 0;
+
+void PUT_data(){
+    String json;
+    DynamicJsonDocument doc(2048);
+    doc["Id"] = id;
+    doc["light"] = light;
+    doc["moisture"] = moisture;
+    doc["temp"] = temp;
+    serializeJson(doc,json);
+
+    const String url = baseUrl + "...";
+    HTTPClient http;
+    http.begin(url);
+    http.addHeader("Content-Type","application/json");
+
+    int httpResponseCode = http.PUT(json);
+    if (httpResponseCode >= 200 && httpResponseCode < 300) {
+        Serial.print("HTTP ");
+        Serial.println(httpResponseCode);
+    }
+    else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+    }
+}
+
 void HarrySong(){
     int size = sizeof(durations) / sizeof(int);
 
